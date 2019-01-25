d0f52ab0fb251395a1dd3f9913f26d3a00530f9a
reichenbergc <chance.reichenberg@claytonhomes.com>
2019-01-24 23:14:59 -0500

    cleaned up files and added BG video

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
+import styled, { createGlobalStyle } from 'styled-components';
+import bgVideo from '../img/bgVideo.mp4';
 
 import Navbar from '../components/Navbar';
+
+const GlobalStyles = createGlobalStyle`
+body{padding: 0;
+  margin: 0;
+}`;
+
+const ContentStyled = styled.div`
+  #myVideo {
+    position: fixed;
+    right: 0;
+    height: 100vh;
+  }
+
+  .overlay {
+    position: fixed;
+    height: 100vh;
+    width: 100vw;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+
+  background-color: rgb(200, 200, 200);
+  width: 100vw;
+  height: 100vh;
+`;
 
 const TemplateWrapper = ({ children }) => (
   <StaticQuery
@@ -17,7 +43,7 @@ const TemplateWrapper = ({ children }) => (
       }
     `}
     render={data => (
-      <div>
+      <ContentStyled>
         <Helmet>
           <html lang="en" />
           <title>{data.site.siteMetadata.title}</title>
@@ -31,9 +57,14 @@ const TemplateWrapper = ({ children }) => (
           <meta property="og:url" content="/" />
           <meta property="og:image" content="/img/og-image.jpg" />
         </Helmet>
+        <GlobalStyles />
+        <video autoPlay muted loop id="myVideo" playbackRate={100}>
+          <source src={bgVideo} type="video/mp4" />
+        </video>
+        <div className="overlay" />
+        <div className="content">{children}</div>
         <Navbar />
-        <div>{children}</div>
-      </div>
+      </ContentStyled>
     )}
   />
 );
