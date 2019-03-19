621ad02dc4563041734cd28c447ff9a3d0fab6c7
Chance Reichenberg <chance.reichenberg@gmail.com>
2019-03-18 20:07:24 -0400

    better social stuffs

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -220,6 +220,7 @@ class TemplateWrapper extends Component {
                 property="og:title"
                 content={data.site.siteMetadata.title}
               />
+              <meta property="og:description" content={data.site.siteMetadata.description}/>
               <meta property="og:url" content="https://chanceontravel.com" />
               <meta property="og:image" content="/img/test-landscape.jpg" />
               <link
